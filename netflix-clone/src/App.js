3e796a53df12ec0c2cc1caaeaa83ad93ee67548b
chinmay021 <chinmaykumar021@gmail.com>
2020-10-17 18:12:41 +0530

    deployed to firebase

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Nav />
       <Banner />
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} /> {/* by default isLargeRow = true*/}
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow /> {/* by default isLargeRow = {true}*/}
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
