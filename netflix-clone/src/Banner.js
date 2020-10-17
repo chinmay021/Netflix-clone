3c9754372c93e23c3c50ebb7dcee536a1f68f933
chinmay021 <chinmaykumar021@gmail.com>
2020-10-17 21:40:21 +0530

    changed banner.js

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -26,7 +26,6 @@ function Banner() {
                 backgroundSize: "cover",
                 backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
                 backgroundPosition: "center center ",
-                backgroundRepeat: "no-repeat"
             }}>
                 <div className="banner__contents">
                     <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
